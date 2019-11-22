2e1b5835c147f0cdbf6c8287d0c15606046b642c
Jean-Baptiste <jeanbaptiste.martinot@lapiscine.pro>
2019-11-22 15:11:18 +0100

    Second commit library

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -7,16 +7,21 @@ use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+//création d'une class controleur avec un extends pour hériter des class de l'AbstractController
 class AuthorController extends AbstractController
 {
     /**
+     * Annotation pour définir ma route
      * @Route("/author_list", name="author_list");
      */
 
+    //méthode qui permet de faire "un select" en BDD d'un id dans ma table Author
     public function AuthorList(AuthorRepository $authorRepository)
     {
         $author = $authorRepository->find(2);
 
+        //méthode render sui permet d'afficher mon fichier html.twig, et le résultat de ma requête SQL
         return $this->render('author.html.twig', [
             'author' => $author
         ]);
--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -14,10 +14,14 @@ class BooksController extends AbstractController
      * @Route("/book_list", name="book_list");
      */
 
+    //méthode qui permet de faire "un select" en BDD de l'ensemble de mes champs dans ma table Book
     public function BooksList(BookRepository $bookRepository)
     {
+        //J'utilise le repository de book pour pouvoir selectionner tous les élèments de ma table book
+        //Les repositorys en général servent à faire les requêtes select dans les tables
         $books = $bookRepository->findAll();
 
+        //méthode render sui permet d'afficher mon fichier html.twig, et le résultat de ma requête SQL
         return $this->render('book.html.twig', [
             'books' => $books
         ]);
