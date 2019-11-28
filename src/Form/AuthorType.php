1ff5656322ce6759a7df561cd573256a53c0e9aa
Jean-Baptiste <jeanbaptiste.martinot@lapiscine.pro>
2019-11-28 09:43:38 +0100

    Ajout de formulaire symfony

---
+++ src/Form/AuthorType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AuthorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Nom',
+                'required' => true
+            ])
+            ->add('firstName', null, [
+                'label' => 'Prénom',
+                'required' => true
+            ])
+            ->add('birthDate', DateType::class, [
+                'years' => range(date('Y'), date('Y') - 500),
+                'label' => 'Date de naissance',
+                'required' => true
+            ])
+            ->add('deathDate', DateType::class, [
+                'years' => range(date('Y'), date('Y') - 500),
+                'label' => 'Date de décès',
+                'required' => true
+            ])
+            ->add('biography', TextareaType::class, [
+                'label' => 'Biographie',
+                'required' => true
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
